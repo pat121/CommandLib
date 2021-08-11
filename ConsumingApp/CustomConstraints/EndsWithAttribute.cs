0a650d26fc40e8a62ca4b1c08aa38f23e60e5ee0
pat121 <patrick@kenspeegle.com>
2021-08-11 13:59:47 -0500

    Added the ConsumingApp.CustomConstraints namespace, which demonstrates how to make custom constraints by subclassing ConstraintAttribute

---
+++ ConsumingApp/CustomConstraints/EndsWithAttribute.cs
@@ -0,0 +1,26 @@
+﻿using CommandLib.Constraints;
+
+namespace ConsumingApp.CustomConstraints
+{
+    /// <summary>
+    /// This example constraint makes sure that an argument ends with a specified substring.
+    /// </summary>
+    public class EndsWithAttribute : ConstraintAttribute
+    {
+        private string _suffix;
+
+        public EndsWithAttribute(string suffix)
+        {
+            _suffix = suffix;
+        }
+
+        public override string GetFailureMessage()
+        {
+            return $"The given string did not end with '{_suffix}'.";
+        }
+        public override bool MeetsConstraint(string argument)
+        {
+            return argument.EndsWith(_suffix);
+        }
+    }
+}
---
+++ ConsumingApp/CustomConstraints/MatchesOneAttribute.cs
@@ -0,0 +1,30 @@
+﻿using CommandLib.Constraints;
+
+namespace ConsumingApp.CustomConstraints
+{
+    /// <summary>
+    /// This example constraint makes sure that an argument is contained in a list of pre-approved values.
+    /// If the argument isn't in the list, the constraint is not met and the command isn't executed.
+    /// </summary>
+    public class MatchesOneAttribute : ConstraintAttribute
+    {
+        private readonly string[] _options;
+
+        public MatchesOneAttribute(params string[] options)
+        {
+            _options = options;
+        }
+
+        public override string GetFailureMessage()
+        {
+            return string.Format("The following values are allowed: {0}", string.Join(", ", _options));
+        }
+        public override bool MeetsConstraint(string argument)
+        {
+            foreach (var item in _options)
+                if (item == argument)
+                    return true;
+            return false;
+        }
+    }
+}
